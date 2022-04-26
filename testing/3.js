cc7c239c8d3d1c546207c0b3aa422fde440fc0ae
hhe haha <89916615+hocsinhgioitoan@users.noreply.github.com>
2022-04-26 10:42:32 +0000

    Fix all

--- testing/3.js
+++ testing/3.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const SourceBin = require('sourcebin-wrapper');
 axios.get('https://raw.githubusercontent.com/hocsinhgioitoan/Mutil-tool/main/version.json').then(resp => {
-console.log(resp.data.patch)
+console.log(resp.data)
 
 
 });
