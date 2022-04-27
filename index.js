75efdcfe1a9eac6f16d0b7a3683507b55d0e7d4d
hhe haha <89916615+hocsinhgioitoan@users.noreply.github.com>
2022-04-27 11:28:14 +0000

    Patch-0.3

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const {
     checkUpdatePackage,
     mobileStatus,
     syncStatus,
-    notice
+    notice,
+    updateTool
 } = require("./config/mainConfig.json")
 const {
     version,
@@ -14,11 +15,13 @@ const {
     name,
     author,
     patch_version
+
 } = require("./package.json")
 var colors = require('colors');
 const {
     logger
 } = require("./utils/logger");
+
 const online = require("./utils/online.js")
 const fs = require('fs');
 const client = new Client({
@@ -33,28 +36,33 @@ const client = new Client({
 const axios = require('axios');
 const SourceBin = require('sourcebin-wrapper');
 
-
-axios.get('https://raw.githubusercontent.com/hocsinhgioitoan/Mutil-tool/main/version.json').then(resp => {
-    if (resp.data.version !== version) {
-        logger.warn("There is a new update, please update to the new version: ".red + resp.data.version.green)
-        logger.update(`Info update: ${resp.data.version.green}
+if (updateTool === true) {
+    axios.get('https://raw.githubusercontent.com/hocsinhgioitoan/Mutil-tool/main/version.json').then(resp => {
+        if (resp.data.version !== version) {
+            logger.warn("There is a new update, please update to the new version: ".red + resp.data.version.green)
+            logger.update(`Info update: ${resp.data.version.green}
 Description:
 ${resp.data.version_description}
 Download here: https://github.com/hocsinhgioitoan/Mutil-tool/releases/
 Clone replit: https://replit.com/github/hocsinhgioitoan/Mutil-tool
 `)
-    } else if (resp.data.patch !== patch_version) {
-        logger.warn("There is a new patch update, please update to the new version: ".red + resp.data.patch.green)
-        logger.update(`Info patch update: ${resp.data.patch.green}
+        } else if (resp.data.patch !== patch_version) {
+            if (resp.data.patch_required === true) {
+                logger.warn("There is a new patch update, please update to the new version: ".red + resp.data.patch.green)
+                logger.update(`Info patch update: ${resp.data.patch.green}
 Description:
 ${resp.data.patch_description}
 Download here: https://github.com/hocsinhgioitoan/Mutil-tool/releases/
 Clone replit: https://replit.com/github/hocsinhgioitoan/Mutil-tool
 `)
-    } else {
-        logger.info(`No new updates at the moment, feel free`.green)
-    }
-});
+            }
+
+        } else {
+            logger.info(`No new updates at the moment, feel free`.green)
+        }
+    });
+}
+
 
 logger.info(`
 ███╗░░░███╗██╗░░░██╗████████╗██╗██╗░░░░░░██████╗  ████████╗░█████╗░░█████╗░██╗░░░░░
@@ -63,7 +71,7 @@ logger.info(`
 ██║╚██╔╝██║██║░░░██║░░░██║░░░██║██║░░░░░░╚═══██╗  ░░░██║░░░██║░░██║██║░░██║██║░░░░░
 ██║░╚═╝░██║╚██████╔╝░░░██║░░░██║███████╗██████╔╝  ░░░██║░░░╚█████╔╝╚█████╔╝███████╗
 ╚═╝░░░░░╚═╝░╚═════╝░░░░╚═╝░░░╚═╝╚══════╝╚═════╝░  ░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝
-Made by ${author} Verion v${version} - ${description}
+Made by ${author} Verion v${version} - Patch: ${patch_version}- ${description}
 `.blue)
 
 function init() {
@@ -73,8 +81,9 @@ function init() {
 init()
 process.on('unhandledRejection', error => {
     if (notice === true) {
+
         if (!process.env.WH_URL) {
-            return logger.error("Invalid Webhook Link. Please check here https://github.com/hocsinhgioitoan/Mutil-tool#env-required")
+            return logger.error("Invalid Webhook Link. Please check here Fhttps://github.com/hocsinhgioitoan/Mutil-tool#env-required")
         } else {
             console.error(error)
             const WebHookClient = new Discord.WebhookClient({
@@ -86,6 +95,9 @@ process.on('unhandledRejection', error => {
         }
     }
 });
+if (!process.env["token"] || process.env["TOKEN"]) {
+    return logger.error("Missing token please enter token in secret tab to run this selfbot")
+}
 client.login(process.env["token"] || process.env["TOKEN"]); // Don't paste your token here 
 function loadEvent() {
     fs.readdirSync('./src/events').forEach((category) => {
--- utils/online.js
+++ utils/online.js
@@ -1,4 +1,6 @@
 const {logger} = require("./logger.js")
+const {linkWeb} = require("../config/mainConfig.json")
+const axios = require("axios")
 function online() {
     const express = require('express');
     const app = express();
@@ -8,5 +10,11 @@ function online() {
     app.listen(3000, () => {
         logger.info('Web is used to host 24/7 online ');
     });
+    setInterval(function() {
+      axios.get(linkWeb).then(res=>{
+        logger.heartbeat("Pinged web")
+      })
+      
+    },1000*60*1)
 }  
 module.exports = online
