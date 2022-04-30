f70bdf32d67891866a8f64e99f2c68e5a6a76758
hhe haha <89916615+hocsinhgioitoan@users.noreply.github.com>
2022-04-30 04:25:54 +0000

    Change version 2.2.0 -> 2.1.0

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const RPC = require('discord-rpc-contructor');
 // Bot ID
 const {RP_ApplicationId } = require("./config/statusConfig.json")
-RPC.getRpcImages(RP_ApplicationId).then(console.log);
+RPC.getRpcImages(RP_ApplicationId).then(console.log)
